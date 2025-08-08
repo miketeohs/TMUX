72ece50a4dfb8156f0d77c3a6b912304f860b741
miketeohs <miketeohs@gmail.com>
2025-08-08 23:21:27 +0800

    Starttmux.sh updated

--- Starttmux.sh
+++ Starttmux.sh
@@ -30,3 +30,41 @@ done
 # tmux ls
 # tmux a -t S1
 # tmux a -t S2
+#
+# Additonal stuff
+# Starttmux() {
+##for S in S1 S2
+for S in S1
+do
+    tmux new -d -s $S
+    tmux select-pane -t .1 -T Dockers
+    tmux splitw
+    tmux select-pane -t .2
+    tmux split-window -h
+    tmux select-pane -t .3
+    tmux split-window -h
+    tmux select-pane -t 1 -T Main
+    tmux select-pane -t 2 -T docker-ps
+    tmux select-pane -t 3 -T docker-ps-a
+    tmux select-pane -t 4 -T docker-images
+    tmux select-pane -t 1
+    tmux send-keys -t 1 'echo "Dockers World"' C-m
+#    tmux send-keys -t 2 watch -n 2 'docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Status}}"' C-m
+#    tmux send-keys -t 3 watch -n 2 'docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.Status}}"' C-m
+#    tmux send-keys -t 3 watch -n 2 'docker image list --format "table {{.ID}}\t{{.Repository}}\t{{.Tag}}\t{{.Size}}"' C-m
+done
+#}
+
+#StartHtmux() {
+#     tmux new -d -s S1
+#     tmux select-pane -t 1 -T Main
+#     tmux split-window -h
+#     tmux select-pane -t 2 -T W2
+#     tmux splitw
+#     tmux select-pane -t 3 -T W3
+#     tmux splitw
+#     tmux select-pane -t 4 -T W4
+#     tmux set -g pane-border-status bottom
+# }
+
+sudo mount --make-shared /
